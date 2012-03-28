eadf0bdaa68de78f6747ec3ec23df29128e32c87
Florian Nitschmann <f.nitschmann@media-n.net>
2012-03-28 12:53:40 +0200

    Created Version 1.0

---
+++ src/heronverfahren/HeronVerfahren.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package heronverfahren;
+
+/**
+ *
+ * @author Florian Nitschmann
+ */
+public class HeronVerfahren {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interface view = new Interface();
+        view.setVisible(true);
+    }
+}
